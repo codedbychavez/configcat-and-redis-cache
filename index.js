f51a650e1b9450016c0c203d6c33d4b3b0a0253d
Chavez Harris <codedbychavez@gmail>
2023-03-12 12:15:07 -0300

    Init

---
+++ index.js
@@ -0,0 +1,12 @@
+function MyRedisCache() {}
+MyCustomCache.prototype.set = function (key, config) {
+  // `key` [string] - a unique cache key
+  // `config` [object (ProjectConfig)] - configcat's cache config item
+  // insert your cache write logic here
+};
+MyCustomCache.prototype.get = function (key) {
+  // `key` [string] - a unique cache key
+  // insert your cache read logic here
+};
+
+export default MyRedisCache;
